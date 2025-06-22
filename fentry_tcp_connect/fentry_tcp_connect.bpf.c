9faed826707b9fa16da94660b6ea34bebc3b5cde
Kerolt <kerolt@qq.com>
2025-06-22 16:10:17 +0800

    fentry学习：tcp_connect

---
+++ fentry_tcp_connect/fentry_tcp_connect.bpf.c
@@ -0,0 +1,53 @@
+//go:build ignore
+
+#include "../vmlinux/vmlinux.h"
+
+#include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#define AF_INET 2
+#define TASK_COMM_LEN 16
+
+char __license[] SEC("license") = "Dual MIT/GPL";
+
+// 定义一个 eBPF ringbuf map，用于在内核空间和用户空间之间高效传递事件数据。
+struct {
+    __uint(type, BPF_MAP_TYPE_RINGBUF);
+    __uint(max_entries, 1 << 24);
+    __type(value, struct event);
+} events SEC(".maps");
+
+// 用于捕获和传递 TCP 连接事件的关键信息，通常在 eBPF 程序监控网络活动时使用。
+// 当程序检测到新的 TCP连接时，会将连接的详细信息打包到这个结构体中，然后发送给用户空间程序进行处理。
+struct event {
+    u8 comm[16];
+    __u16 sport;
+    __be16 dport;
+    __be32 saddr;
+    __be32 daddr;
+};
+
+SEC("fentry/tcp_connect")
+int BPF_PROG(tcp_connect, struct sock* sk) {
+    if (sk->__sk_common.skc_family != AF_INET) {
+        return 0;
+    }
+
+    struct event* tcp_info;
+    tcp_info = bpf_ringbuf_reserve(&events, sizeof(struct event), 0);
+    if (!tcp_info) {
+        return 0;
+    }
+
+    tcp_info->saddr = sk->__sk_common.skc_rcv_saddr;
+    tcp_info->daddr = sk->__sk_common.skc_daddr;
+    tcp_info->dport = sk->__sk_common.skc_dport;
+    tcp_info->sport = bpf_htons(sk->__sk_common.skc_num);
+
+    bpf_get_current_comm(&tcp_info->comm, TASK_COMM_LEN);
+
+    bpf_ringbuf_submit(tcp_info, 0);
+
+    return 0;
+}
---
+++ fentry_tcp_connect/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/cilium/ebpf/link"
+	"github.com/cilium/ebpf/ringbuf"
+	"github.com/cilium/ebpf/rlimit"
+)
+
+//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags "-O2 -g -Wall -Werror -D__TARGET_ARCH_x86" bpf fentry_tcp_connect.bpf.c
+
+func main() {
+	stopper := make(chan os.Signal, 1)
+	signal.Notify(stopper, os.Interrupt, syscall.SIGTERM)
+
+	// 移除内存限制
+	if err := rlimit.RemoveMemlock(); err != nil {
+		log.Fatal(err)
+	}
+
+	objs := bpfObjects{}
+	if err := loadBpfObjects(&objs, nil); err != nil {
+		log.Fatalf("loading bpf objects: %v", err)
+	}
+	defer objs.Close()
+
+	// SEC("kprobe/do_unlinkat")
+	link, err := link.AttachTracing(link.TracingOptions{
+		Program: objs.TcpConnect,
+	})
+	if err != nil {
+		log.Fatalf("linking Fentry: %v", err)
+	}
+	defer link.Close()
+	rd, err := ringbuf.NewReader(objs.Events)
+	if err != nil {
+		log.Fatalf("opening ringbuf reader: %s", err)
+	}
+	defer rd.Close()
+
+	go func() {
+		<-stopper
+
+		if err := rd.Close(); err != nil {
+			log.Fatalf("closing ringbuf reader: %s", err)
+		}
+	}()
+
+	log.Printf("%-16s %-15s %-6s -> %-15s %-6s",
+		"Comm",
+		"Src addr",
+		"Port",
+		"Dest addr",
+		"Port",
+	)
+
+	// 不断从 ringbuf 中读取事件、解析事件并打印相关信息，直到接收到终止信号
+	var event bpfEvent
+	for {
+		record, err := rd.Read()
+		if err != nil {
+			if errors.Is(err, ringbuf.ErrClosed) {
+				log.Println("received signal, exiting..")
+				return
+			}
+			log.Printf("reading from reader: %s", err)
+			continue
+		}
+
+		// Parse the ringbuf event entry into a bpfEvent structure.
+		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.BigEndian, &event); err != nil {
+			log.Printf("parsing ringbuf event: %s", err)
+			continue
+		}
+
+		log.Printf("%-16s %-15s %-6d -> %-15s %-6d",
+			event.Comm,
+			intToIP(event.Saddr),
+			event.Sport,
+			intToIP(event.Daddr),
+			event.Dport,
+		)
+	}
+}
+
+// intToIP converts IPv4 number to net.IP
+func intToIP(ipNum uint32) net.IP {
+	ip := make(net.IP, 4)
+	binary.BigEndian.PutUint32(ip, ipNum)
+	return ip
+}
